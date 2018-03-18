57715afe57936a638ada9939ca7bb5d152a3defe
Sammich <oel333@yahoo.com>
2018-03-18 18:59:39 -0500

    HTML structure
    1. remove some unnecessary tags
    
    2. moved script.js tag to bottom of index.html so the HTML loads first
    
    3. remove git error codes from the code
    
    4. replaced radio tags with select to make the card smaller (still needs
    to be smaller)
    
    5. removed jQuery for now, image uploading using plain JS
    
    6. some CSS changes just to make things layout correctly

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-<<<<<<< HEAD
 function myFunction() {
     document.getElementById("frm1").submit();
 }
 
 //this code is for the image uploading
-$("#image").change(function(){
-        readURL(this);
-   
-    });
+document.getElementById('image').addEventListener('change', function () {
+    console.log('test')
+    readURL(this);
+});
 
 function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            
-            reader.onload = function (e) {
-                $('#profile').attr('src', e.target.result);
-            }
-            
-            reader.readAsDataURL(input.files[0]);
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            document.getElementById('profile').src = e.target.result;
         }
+            
+        reader.readAsDataURL(input.files[0]);
     }
-=======
-
->>>>>>> 27350ea810d61a15750c07b1f44ecc951cae9789
+}
