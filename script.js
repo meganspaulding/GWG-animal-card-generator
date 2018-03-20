a81826cdd36395ef13f8e92fe8e5a69a41ddcc2f
MeganSpauldingCreative <35813751+MeganSpauldingCreative@users.noreply.github.com>
2018-03-19 19:46:36 -0500

    Revert "basic HTML structure, start of JavaScript"

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-//this code is for the image uploading
-document.getElementById('image').addEventListener('change', function () {
-    readURL(this);
-});
-
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            document.getElementById('profile').src = e.target.result;
-        }
-            
-        reader.readAsDataURL(input.files[0]);
-    }
+<<<<<<< HEAD
+function myFunction() {
+    document.getElementById("frm1").submit();
 }
 
-document.getElementById('addCard').addEventListener('click', function () {
-    document.getElementById('cardMakeOverlay').style.display = 'block';
-})
+//this code is for the image uploading
+$("#image").change(function(){
+        readURL(this);
+   
+    });
+
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            
+            reader.onload = function (e) {
+                $('#profile').attr('src', e.target.result);
+            }
+            
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+=======
+
+>>>>>>> 27350ea810d61a15750c07b1f44ecc951cae9789
